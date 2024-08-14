ff1d10b3676417094b27460210e5e10b0488cec3
Lorenzo <hello@lorenzo0111.me>
2024-08-14 16:19:56 +0200

    App setup - Created vite app with react - Added capacitor - Added shadcn

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "me.lorenzo0111.bank",
+  appName: "Bank",
+  webDir: "dist",
+};
+
+export default config;
---
+++ app/src/main.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("No root element");
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
